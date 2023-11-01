814282d81208659c35c867f26a978aad70036a77
toan.dang <toan.dang@xomad.com>
2023-11-01 15:08:52 +0700

    [update]

--- src/app/summary-info/summary-info.component.ts
+++ src/app/summary-info/summary-info.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SummaryInfoComponent {
     textSummary = ` 6 năm-một hành trình không quá dài, nhưng đủ để chúng mình cùng nhau trải qua tất cả những cung bậc cảm xúc.
-    Em và anh đã cùng nhau trưởng thành, cùng nhau lớn lên, cùng nhau rèn giũa để trở thành phiên bản tôt nhất của chính mình.
+    Em và anh đã cùng nhau trưởng thành, cùng nhau lớn lên, cùng nhau rèn giũa để trở thành phiên bản tốt nhất của chính mình.
     Và đến một ngày, chúng mình dành cho nhau phiên bản đẹp đẽ ấy, để cùng nhau bước sang một chương mới, viết tiếp câu chuyện tình yêu tuyệt vời.
-    Chúng mình đã quyết định chính thức về chung môt nhà, đi chung một con đường. Chúng mình đã chờ đợi suốt 6 năm cho khoảnh khắc thiêng liêng này, và chúng mình đã sẵn sàng sánh bước cùng nhau về sau mãi mãi.`
+    Chúng mình đã quyết định chính thức về chung một nhà, đi chung một con đường. Chúng mình đã chờ đợi suốt 6 năm cho khoảnh khắc thiêng liêng này, và chúng mình đã sẵn sàng sánh bước cùng nhau về sau mãi mãi.`
 }
